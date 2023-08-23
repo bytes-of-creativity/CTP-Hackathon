2222987f632cedde65fe509d616ac99984134b88
hninl23 <h.lwin0440@gmail.com>
2023-08-23 12:21:20 -0400

    KAN-10 added  navbar component

--- adsh/src/components/ChallengeWinners.js
+++ adsh/src/components/ChallengeWinners.js
@@ -7,22 +7,23 @@ const ChallengeWinners = () => {
         <>
             <div className="global-style"></div>
             <div className="background-container">
-                <h1 className="winner-title">Challenge Winners</h1>
-                <div className="img-container">
-                    <div className="winner-user-container">
-                        <img className="img-config" src={PlaceholderView} alt="Winner 1" />
-                        Winner 1 User
+                    <h1 className="winner-title">Challenge Winners</h1>
+                    <div className="img-container">
+                        <div className="winner-user-container">
+                            <img className="img-config" src={PlaceholderView} alt="Winner 1" />
+                            Winner 1 User
+                        </div>
+                        <div className="winner-user-container">
+                            <img className="img-config" src={PlaceholderView} alt="Winner 2" />
+                            Winner 2 User
+                        </div>
+                        <div className="winner-user-container">
+                            <img className="img-config" src={PlaceholderView} alt="Winner 3" />
+                            Winner 3 User
+                        </div>
                     </div>
-                    <div className="winner-user-container">
-                        <img className="img-config" src={PlaceholderView} alt="Winner 2" />
-                        Winner 2 User
-                    </div>
-                    <div className="winner-user-container">
-                        <img className="img-config" src={PlaceholderView} alt="Winner 3" />
-                        Winner 3 User
-                    </div>
-                </div>
-                <h2 className="previous-prompt">Previous Prompt:</h2>
+                    <h2 className="previous-prompt">Previous Prompt:</h2>
+                
             </div>
         </>
     );
--- adsh/src/components/NavProfile.js
+++ adsh/src/components/NavProfile.js
@@ -1,14 +1,74 @@
-import React from 'react'
-import { styled } from 'styled-components';//import used to make inline css easier
+import React, {useState} from 'react'
+
+// import { styled } from 'styled-components';//import used to make inline css easier
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';//import used to add icons, need to install package
-import { faEye } from '@fortawesome/free-solid-svg-icons';//import used to add the eye icon, need to install package
+import { faUser } from '@fortawesome/free-solid-svg-icons';//import used to add the eye icon, need to install package
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faRobot} from '@fortawesome/free-solid-svg-icons';
+import '../styles/NavProfile.css';
 
 const NavProfile = () => {
     return (
         <div>
+        <NavBar>
+            <NavItems id='logo' icon= {faRobot} width="20px" height="20px">
+            </NavItems>
+            <div className='right-nav-items'>
+            <NavItems id='right-logo' icon= {faPlus} width="20px" height="20px">
+                <DropDown>
+                    <DropDownItem link="#">Post/Submit</DropDownItem>
+                </DropDown>
+            </NavItems>
+            <NavItems id='right-logo' icon= {faUser} width="20px" height="20px">
+                <DropDown>
+                    <DropDownItem link="#"> My Profile</DropDownItem>
+                    <DropDownItem link="#">Setting</DropDownItem>
+                    <DropDownItem link="#">Log Out</DropDownItem>
+                </DropDown>
+            </NavItems>
+            </div>
+        </NavBar>
+        </div>
+    );
+} 
 
+const NavBar = (props) => {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-item-container'> { props.children }</div> 
+        </nav>  
+    )
+}
+
+const NavItems = (props) => {
+    const [open, setOpen] = useState(false);
+    return (
+        <li className='nav-item'>
+            <a href={props.link}  className='icon-button' onClick={() => setOpen(!open)} > 
+                <FontAwesomeIcon icon={props.icon} />
+            </a>
+            {open && props.children}
+            
+        </li>
+    )
+}
+
+const DropDown = (props) => {
+    return (
+        <div className='dropdown'>
+            {props.children}
         </div>
     )
-} 
+}
+
+const DropDownItem = (props) => {
+    return (
+        <a href={props.link} className='dropdown-item'>
+            {props.children}
+        </a>
+    )
+}
+
+
 
 export default NavProfile 
