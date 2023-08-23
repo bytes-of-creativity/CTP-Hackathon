5e8505d33d5133014b9d740182a8c1d42d4ccfc5
DFLotus <deondraefrancis101@gmail.com>
2023-08-23 14:43:33 -0400

    KAN-8: Default buttons and empty view

--- adsh/src/pages/Home.js
+++ adsh/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavProfile from '../components/NavProfile'
 import ChallengeWinners from "../components/ChallengeWinners";
 import CurrentChallenge from "../components/CurrentChallenge";
-import PostView from "../components/PostView";
+import PostButtons from "../components/PostButtons/PostButtons";
 
 const Home = () => {
     return (
@@ -10,7 +10,7 @@ const Home = () => {
             <NavProfile/>
             <ChallengeWinners/>
             <CurrentChallenge/>
-            <PostView/>
+            <PostButtons/>
         </div>
     )
 }
