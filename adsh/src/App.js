a8bede380a69f1582bd43cbfc913d3411c0e04f8
hninl23 <h.lwin0440@gmail.com>
2023-08-25 14:48:50 -0400

    Added Profile Page

--- adsh/src/App.js
+++ adsh/src/App.js
@@ -1,12 +1,13 @@
 import './styles/App.css';
 import HomeContainer from './pages/HomeContainer';
 import LogInContainer from './pages/LogInContainer';
+import ProfilePageContainer from './pages/ProfilePageContainer';
 import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeContainer/>} />
+      <Route path="/" element={<ProfilePageContainer/>} />
       <Route path="/login" element={<LogInContainer/>} />
     </Routes>
   )
